ad8ce2178e78f614ffc8372709c7c676afa4e8ad
victoriarspada <victoria.spada@canada.ca>
2021-09-17 15:20:10 -0400

    Decode features from bytes to UTF-8 string before running json.loads()
    Decode features from bytes to UTF-8 string before running json.loads()

--- pywoudc/__init__.py
+++ pywoudc/__init__.py
@@ -297,7 +297,7 @@ class WoudcClient(object):
             LOGGER.info('Emitting raw GeoJSON response')
             return features.read()
         LOGGER.info('Emitting GeoJSON features as list')
-        return json.loads(features.read())
+        return json.loads(features.read().decode('utf-8'))
 
 
 def date2string(dateval, direction='begin'):
